3d0a014cd6190acdb0186c5d0b97a5669f8c80f2
Lion786420 <thapaaniket12345@gmail.com>
2023-06-24 13:23:48 +0545

    Done full game of 5

--- index.js
+++ index.js
@@ -8,9 +8,7 @@ const getComputerChoice=()=>{
         return "scissors";
     }
 }
-let playerSelection=prompt("Enter your choice");
-console.log(playerSelection);
-let ComputerChoice=getComputerChoice();
+let playerScore=0,computerScore=0;
 const round=(playerSelection,ComputerChoice)=>{
     if(playerSelection.toLowerCase()=='rock' && ComputerChoice=='rock'){
         console.log("Draw");
@@ -20,16 +18,38 @@ const round=(playerSelection,ComputerChoice)=>{
         console.log("Draw");
     }else if(playerSelection.toLowerCase()=='rock' && ComputerChoice=='paper'){
         console.log("You lose. Paper beats Rock");
+        computerScore++;
     }else if(playerSelection.toLowerCase()=='rock' && ComputerChoice=='scissors'){
         console.log("You win. Rock beats Scissors");
+        playerScore++;
     }else if(playerSelection.toLowerCase()=='scissors' && ComputerChoice=='rock'){
         console.log("You lose. Rock beats Paper");
-    }else if(playerSelection.toLowerCase()=='scissors' && ComputerChoice=='rock'){
+        computerScore++;
+    }else if(playerSelection.toLowerCase()=='scissors' && ComputerChoice=='paper'){
         console.log("You win. Scissors beats paper");
+        playerScore++;
     }else if(playerSelection.toLowerCase()=='paper' && ComputerChoice=='rock'){
         console.log("You win. Paper beats rock");
-    }else{
+        playerScore++;
+    }else if(playerSelection.toLowerCase()=='paper' && ComputerChoice=='scissors'){
         console.log("You lose. Scissors beats paper");
+        computerScore++;
+    }else{
+        console.log("Invalid choice");
     }
 }
-round(playerSelection,ComputerChoice);
+const game=()=>{
+    for(let i=1;i<=5;i++){
+        let playerSelection=prompt("Enter your choice");   
+        let ComputerChoice=getComputerChoice();
+        round(playerSelection,ComputerChoice);
+    }
+    if(playerScore>computerScore){
+        console.log("YOU WIN THE GAME");
+    }else if(computerScore>playerScore){
+        console.log("YOU LOSE THE GAME");
+    }else{
+        console.log("GAME DRAWN");
+    }
+}
+game();
