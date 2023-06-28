50e7e7bc09e2dbe1c161278a30b246cc2a10c448
Lion786420 <thapaaniket12345@gmail.com>
2023-06-28 07:22:23 +0545

    Add Final result UI

--- index.js
+++ index.js
@@ -12,28 +12,28 @@ let playerScore=0,computerScore=0;
 const round=(playerSelection)=>{
     ComputerChoice=getComputerChoice();
     if(playerSelection.toLowerCase()=='rock' && ComputerChoice=='rock'){
-        result.textContent="Draw";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nDraw `;
     }else if(playerSelection.toLowerCase()=='scissors' && ComputerChoice=='scissors'){
-        result.textContent="Draw";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nDraw`;
     }else if(playerSelection.toLowerCase()=='paper' && ComputerChoice=='paper'){
-        result.textContent="Draw";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nDraw`;
     }else if(playerSelection.toLowerCase()=='rock' && ComputerChoice=='paper'){
-        result.textContent="You lose. Paper beats Rock";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nYou lose. Paper beats Rock`;
         computerScore++;
     }else if(playerSelection.toLowerCase()=='rock' && ComputerChoice=='scissors'){
-        result.textContent="You win. Rock beats Scissors";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nYou win. Rock beats Scissors`;
         playerScore++;
     }else if(playerSelection.toLowerCase()=='scissors' && ComputerChoice=='rock'){
-        result.textContent="You lose. Rock beats Paper";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nYou lose. Rock beats Paper`;
         computerScore++;
     }else if(playerSelection.toLowerCase()=='scissors' && ComputerChoice=='paper'){
-        result.textContent="You win. Scissors beats paper";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nYou win. Scissors beats paper`;
         playerScore++;
     }else if(playerSelection.toLowerCase()=='paper' && ComputerChoice=='rock'){
-        result.textContent="You win. Paper beats rock";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nYou win. Paper beats rock`;
         playerScore++;
     }else if(playerSelection.toLowerCase()=='paper' && ComputerChoice=='scissors'){
-        result.textContent="You lose. Scissors beats paper";
+        result.textContent=`Computer score: ${computerScore}\nPlayer score: ${playerScore}\nYou lose. Scissors beats paper`;
         computerScore++;
     }else{
         result.textContent="Invalid choice";
@@ -54,4 +54,12 @@ const btn=document.querySelectorAll("button");
 btn.forEach((button)=>button.addEventListener('click',(e)=>{
     let content=e.target.textContent;
     round(content);
+    if(computerScore==5){
+        result.textContent="Computer wins";
+        playerScore=0,computerScore=0
+    }
+    if(playerScore==5){
+        result.textContent="You win";
+        playerScore=0,computerScore=0;
+    }
 }));
